f3355b5e930818c9ec5b6b30fba90a7245eb6a2e
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-02-28 18:03:53 -0500

    Finish operator support in variables
    With the new logic functions, we add support on the Variable object for
    overloading Python's operators. This lets us express logic natively,
    such as:
    
        (a == -b) | (c == -d) & (d ^ -a)
    
    Also expands non-operators such as __str__, __int__, and __bool__.
    
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- cmsh/var.py
+++ cmsh/var.py
@@ -1,4 +1,4 @@
-from .tseitin import *
+from .logic import *
 
 
 class Variable:
@@ -18,16 +18,137 @@ class Variable:
         return self.value
 
     def __add__(self, other):
-        return v_or(self, other)
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_or(self, other)
+
+    def __sub__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_or(self, b_not(other))
 
     def __mul__(self, other):
-        return v_and(self, other)
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_and(self, other)
+
+    def __div__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_and(self, b_not(other))
+
+    def __and__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_and(self, other)
+
+    def __xor__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_xor(self, other)
+
+    def __or__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_or(self, other)
+
+    def __radd__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_or(self, other)
+
+    def __rsub__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_or(self, b_not(other))
+
+    def __rmul__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_and(self, other)
+
+    def __rdiv__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_and(self, b_not(other))
+
+    def __rand__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_and(self, other)
+
+    def __rxor__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_xor(self, other)
+
+    def __ror__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_or(self, other)
+
+    def __lt__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_lt(self, other)
+
+    def __le__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_le(self, other)
+
+    def __eq__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_eq(self, other)
+
+    def __ne__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_ne(self, other)
+
+    def __gt__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_gt(self, other)
+
+    def __ge__(self, other):
+        if not isinstance(other, (Variable, bool)):
+            return NotImplemented
+        return b_ge(self, other)
+
+    def __neg__(self):
+        return b_not(self)
+
+    def __pos__(self):
+        return self
+
+    def __abs__(self):
+        if self.identifier < 0:
+            return b_not(self)
+        return self
+
+    def __hash__(self):
+        return self.identifier.__hash__()
+
+    def __int__(self):
+        return self.identifier
 
     def __str__(self):
+        val = self.get_value()
+        if val is None:
+            return str(self.identifier)
+
         return str(self.get_value())
 
     def __bool__(self):
-        return self.get_value()
+        val = self.get_value()
+        if val is None:
+            msg = "Cannot call __bool__ before calling solve(). "
+            msg += "If trying to negate a var, use -var."
+            raise NotImplementedError(msg)
+
+        return val
 
 
 class NamedVariable(Variable):
@@ -37,21 +158,5 @@ class NamedVariable(Variable):
         super().__init__(model)
         self.name = name
 
-
-class BitVector():
-    variables = []
-    model = None
-
-    def __init__(self, model, width=None, variables=None):
-        self.model = model
-        self.variables = []
-
-        if width and variables:
-            raise ValueError("Cannot specify both width and variables!")
-
-        if width:
-            for i in range(0, width):
-                self.variables.append(self.model.var())
-
-        if variables:
-            self.variables = variables[:]
+    def __str__(self):
+        return self.name + ":" + super().__str__()
