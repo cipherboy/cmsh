a532dbc47ab95f5658f2b6ee7cdd4cce1fdac274
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-03-01 08:08:18 -0500

    Add addition and shift operators to Vectors
    Vectors now include shift and rotation operators, addition operators,
    even/odd operators, and bitwise summation (quantity of all true bits).
    
    Fixes: #1
    
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- cmsh/vec.py
+++ cmsh/vec.py
@@ -1,9 +1,11 @@
 from .array import *
+from .arith import ripple_carry_adder, sum_array, flatten
 from .var import Variable
 
 
 class Vector:
     variables = None
+    count = None
     model = None
     __allowed__ = (bool, int, list, tuple)
 
@@ -26,6 +28,64 @@ class Vector:
         elif isinstance(vector, (list, tuple)):
             self.variables = tuple(vector)[:]
 
+        self.count = len(self.variables)
+
+    def bit_sum(self):
+        return sum_array(self)
+
+    def bit_odd(self):
+        return self.bit_sum()[-1]
+
+    def bit_even(self):
+        return b_not(self.bit_odd())
+
+    def odd(self):
+        return self.variables[-1]
+
+    def even(self):
+        return b_not(self.odd())
+
+    def bit_and(self):
+        return flatten(self, b_and)
+
+    def bit_nand(self):
+        return flatten(self, b_nand)
+
+    def bit_or(self):
+        return flatten(self, b_or)
+
+    def bit_nor(self):
+        return flatten(self, b_nor)
+
+    def bit_xor(self):
+        return flatten(self, b_xor)
+
+    def rotl(self, amount=1):
+        amount = abs(amount) % (self.count+1)
+        new_vec = self.variables[amount:] + self.variables[:amount]
+        return self.model.to_vector(new_vec)
+
+    def rotr(self, amount=1):
+        amount = abs(amount) % (self.count+1)
+        new_vec = self.variables[-amount:] + self.variables[:-amount]
+        return self.model.to_vector(new_vec)
+
+    def shiftl(self, amount=1, filler=False):
+        amount = abs(amount) % (self.count+1)
+        new_vec = self.variables[amount:] + ([filler]*amount)
+        return self.model.to_vector(new_vec)
+
+    def shiftr(self, amount=1, filler=False):
+        amount = abs(amount) % (self.count+1)
+        new_vec = ([filler]*amount) + self.variables[:-amount]
+        return self.model.to_vector(new_vec)
+
+    def __add__(self, other):
+        if not isinstance(other, self.__allowed__):
+            return NotImplemented
+        result, _ = ripple_carry_adder(self, other)
+        return result
+
     def __and__(self, other):
         if not isinstance(other, self.__allowed__):
             return NotImplemented
@@ -40,6 +100,12 @@ class Vector:
         if not isinstance(other, self.__allowed__):
             return NotImplemented
         return l_or(self, other)
+
+    def __radd__(self, other):
+        if not isinstance(other, self.__allowed__):
+            return NotImplemented
+        result, _ = ripple_carry_adder(self, other)
+        return result
 
     def __rand__(self, other):
         if not isinstance(other, self.__allowed__):
@@ -86,7 +152,17 @@ class Vector:
             return NotImplemented
         return l_ge(self, other)
 
+    def __neg__(self):
+        return l_not(self)
 
+    def __pos__(self):
+        return self
+
+    def __abs__(self):
+        return tuple(map(abs, self.variables))
+
+    def __getitem__(self, key):
+        return self.variables[key]
 
     def __hash__(self):
         return self.variables.__hash__()
