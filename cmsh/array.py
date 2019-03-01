8a2d43b886c22c86da9579be0d35992bfa5fd7e5
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-02-28 21:41:31 -0500

    Add wide logic operations
    Wrap binary logic operators to support wider bitwise applications,
    supporting both wrapped (Vector) and raw data types (lists, tuples, and
    ints). This gives us the implementations for implementing operators on
    Vector instances.
    
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

---
+++ cmsh/array.py
@@ -0,0 +1,215 @@
+from .logic import *
+
+
+def __from_arg(arg):
+    # ret: [Variable:bool], model, is_fixed
+    if isinstance(arg, (list, tuple)):
+        return arg, None, True
+    elif isinstance(arg, int):
+        value = list(map(lambda x: bool(int(x)), bin(arg)[2:]))
+        return value, None, False
+    else:
+        return arg.variables, arg.model, True
+
+
+def __validate_size(left, l_fixed, right, r_fixed):
+    l_len = len(left)
+    r_len = len(right)
+
+    if l_fixed and r_fixed and l_len != r_len:
+        raise ValueError("Mismatch sizes: %d vs %d" % (l_len, r_len))
+    elif l_fixed and l_len < r_len:
+        raise ValueError("Value of constant (%d) exceeds size: %d" % (r_len, l_len))
+    elif r_fixed and r_len < l_len:
+        raise ValueError("Value of constant (%d) exceeds size: %d" % (l_len, r_len))
+
+    print(l_len, r_len)
+
+    if l_len < r_len:
+        prefix = [False] * (r_len - l_len)
+        prefix.extend(left)
+        left = prefix
+        l_len = r_len
+    elif r_len < l_len:
+        prefix = [False] * (l_len - r_len)
+        prefix.extend(right)
+        right = prefix
+        r_len = l_len
+
+    return left, right, l_len
+
+
+def l_and(left, right):
+    l_list, l_model, l_fixed = __from_arg(left)
+    r_list, r_model, r_fixed = __from_arg(right)
+    l_list, r_list, length = __validate_size(l_list, l_fixed, r_list, r_fixed)
+    model = l_model or r_model
+
+    result = []
+    have_var = False
+    for index in range(0, length):
+        item = b_and(l_list[index], r_list[index])
+        result.append(item)
+
+        if not isinstance(item, bool):
+            have_var = True
+
+    if have_var:
+        assert(model)
+        return model.to_vector(result)
+
+    return result
+
+
+def l_nand(left, right):
+    l_list, l_model, l_fixed = __from_arg(left)
+    r_list, r_model, r_fixed = __from_arg(right)
+    l_list, r_list, length = __validate_size(l_list, l_fixed, r_list, r_fixed)
+    model = l_model or r_model
+
+    result = []
+    have_var = False
+    for index in range(0, length):
+        item = b_nand(l_list[index], r_list[index])
+        result.append(item)
+
+        if not isinstance(item, bool):
+            have_var = True
+
+    if have_var:
+        assert(model)
+        return model.to_vector(result)
+
+    return result
+
+
+def l_or(left, right):
+    l_list, l_model, l_fixed = __from_arg(left)
+    r_list, r_model, r_fixed = __from_arg(right)
+    l_list, r_list, length = __validate_size(l_list, l_fixed, r_list, r_fixed)
+    model = l_model or r_model
+
+    result = []
+    have_var = False
+    for index in range(0, length):
+        item = b_or(l_list[index], r_list[index])
+        result.append(item)
+
+        if not isinstance(item, bool):
+            have_var = True
+
+    if have_var:
+        assert(model)
+        return model.to_vector(result)
+
+    return result
+
+
+def l_nor(left, right):
+    l_list, l_model, l_fixed = __from_arg(left)
+    r_list, r_model, r_fixed = __from_arg(right)
+    l_list, r_list, length = __validate_size(l_list, l_fixed, r_list, r_fixed)
+    model = l_model or r_model
+
+    result = []
+    have_var = False
+    for index in range(0, length):
+        item = b_nor(l_list[index], r_list[index])
+        result.append(item)
+
+        if not isinstance(item, bool):
+            have_var = True
+
+    if have_var:
+        assert(model)
+        return model.to_vector(result)
+
+    return result
+
+
+def l_not(var):
+    v_list, v_model = __from_arg(var)
+
+    result = []
+    have_var = False
+    for orig in v_list:
+        item = b_not(orig)
+        result.append(item)
+
+        if not isinstance(item, bool):
+            have_var = True
+
+    if have_var:
+        assert(v_model)
+        return v_model.to_vector(result)
+
+    return result
+
+
+def l_xor(left, right):
+    l_list, l_model, l_fixed = __from_arg(left)
+    r_list, r_model, r_fixed = __from_arg(right)
+    l_list, r_list, length = __validate_size(l_list, l_fixed, r_list, r_fixed)
+    model = l_model or r_model
+
+    result = []
+    have_var = False
+    for index in range(0, length):
+        item = b_xor(l_list[index], r_list[index])
+        result.append(item)
+
+        if not isinstance(item, bool):
+            have_var = True
+
+    if have_var:
+        assert(model)
+        return model.to_vector(result)
+
+    return result
+
+
+def l_lt(left, right):
+    l_list, _, l_fixed = __from_arg(left)
+    r_list, _, r_fixed = __from_arg(right)
+    l_list, r_list, length = __validate_size(l_list, l_fixed, r_list, r_fixed)
+
+    if length == 0:
+        return True
+
+    result = b_lt(l_list[0], r_list[0])
+    and_list = True
+    for index in range(1, length):
+        and_list = b_and(and_list, b_eq(l_list[index-1], r_list[index-1]))
+        result = b_or(result, b_and(and_list, b_lt(l_list[index], r_list[index])))
+
+    return result
+
+
+def l_le(left, right):
+    return b_not(l_gt(left, right))
+
+
+def l_eq(left, right):
+    l_list, _, l_fixed = __from_arg(left)
+    r_list, _, r_fixed = __from_arg(right)
+    l_list, r_list, length = __validate_size(l_list, l_fixed, r_list, r_fixed)
+
+    if length == 0:
+        return True
+
+    result = b_eq(l_list[0], r_list[0])
+    for index in range(1, length):
+        result = b_and(result, b_eq(l_list[index], r_list[index]))
+
+    return result
+
+
+def l_ne(left, right):
+    return b_not(l_eq(left, right))
+
+
+def l_gt(left, right):
+    return b_not(b_or(l_lt(left, right), l_eq(left, right)))
+
+def l_ge(left, right):
+    return b_not(l_lt(left, right))
