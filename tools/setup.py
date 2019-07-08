515c928e892c4bb3d22c77f556260e874dbfb9e9
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-07-07 23:45:27 -0500

    Bump version; drop dependency on pycryptosat
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- tools/setup.py
+++ tools/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='cmsh',
-      version='0.2',
+      version='0.3',
       description="High level interface over Mate Soos's pycryptosat from CryptoMiniSat",
       url='http://github.com/cipherboy/cmsh',
       author='Alexander Scheel',
       author_email='alexander.m.scheel@gmail.com',
       license='GPLv2',
       packages=['cmsh'],
-      install_requires=['pycryptosat'],
+      install_requires=[],
       zip_safe=True)
