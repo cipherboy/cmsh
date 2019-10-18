5809a209ea36470ae62e2d96856e6c0109cd7d77
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-10-17 21:23:24 -0400

    Add typing for Variable class
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- python/var.py
+++ python/var.py
@@ -3,6 +3,9 @@ This module includes the Variable class, which represents a single boolean
 variable in a SAT model.
 """
 
+from typing import Optional, Union
+
+
 class Variable:
     """
     The Variable class represents a single variable in the CNF model. Its
@@ -10,13 +13,12 @@ class Variable:
     negative when refering to its negation.
     """
     identifier: int = 0
-    model = None
 
-    def __init__(self, model, identifier):
+    def __init__(self, model, identifier: int):
         self.model = model
         self.identifier = identifier
 
-    def get_value(self):
+    def get_value(self) -> Optional[bool]:
         """
         Get the value of the variable: True, False, or None, and updated to
         reflect the sign of the identifier. If negative, the value is negated.
@@ -26,9 +28,10 @@ class Variable:
         Returns:
             bool: value of the variable after solving.
         """
+        # pylint: disable=protected-access
         return self.model._get_value_(self.identifier)
 
-    def equals(self, other):
+    def equals(self, other) -> bool:
         """
         Implements the behavior of __eq__(self, other); checks whether this
         variable is equal to other. Will not raise a type error.
@@ -52,7 +55,7 @@ class Variable:
             return self.identifier == other.identifier
         return False
 
-    def not_equals(self, other):
+    def not_equals(self, other) -> bool:
         """
         Implements the behavior of __ne__(self, other); checks whether this
         variable is unequal to other. Will not raise a type error.
@@ -68,32 +71,32 @@ class Variable:
         """
         return not self.equals(other)
 
-    def __and__(self, other):
+    def __and__(self, other: Union[bool, 'Variable']) -> Union[bool, 'Variable']:
         if not isinstance(other, (Variable, bool)):
             return NotImplemented
         return b_and(self, other)
 
-    def __xor__(self, other):
+    def __xor__(self, other: Union[bool, 'Variable']) -> Union[bool, 'Variable']:
         if not isinstance(other, (Variable, bool)):
             return NotImplemented
         return b_xor(self, other)
 
-    def __or__(self, other):
+    def __or__(self, other: Union[bool, 'Variable']) -> Union[bool, 'Variable']:
         if not isinstance(other, (Variable, bool)):
             return NotImplemented
         return b_or(self, other)
 
-    def __rand__(self, other):
+    def __rand__(self, other: Union[bool, 'Variable']) -> Union[bool, 'Variable']:
         if not isinstance(other, (Variable, bool)):
             return NotImplemented
         return b_and(self, other)
 
-    def __rxor__(self, other):
+    def __rxor__(self, other: Union[bool, 'Variable']) -> Union[bool, 'Variable']:
         if not isinstance(other, (Variable, bool)):
             return NotImplemented
         return b_xor(self, other)
 
-    def __ror__(self, other):
+    def __ror__(self, other: Union[bool, 'Variable']) -> Union[bool, 'Variable']:
         if not isinstance(other, (Variable, bool)):
             return NotImplemented
         return b_or(self, other)
@@ -142,29 +145,29 @@ class Variable:
             return NotImplemented
         return b_ge(self, other)
 
-    def __neg__(self):
+    def __neg__(self) -> Union[bool, 'Variable']:
         return b_not(self)
 
-    def __pos__(self):
+    def __pos__(self) -> Union[bool, 'Variable']:
         return self
 
-    def __abs__(self):
+    def __abs__(self) -> int:
         return abs(self.identifier)
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return self.identifier.__hash__()
 
-    def __int__(self):
+    def __int__(self) -> int:
         return self.identifier
 
-    def __str__(self):
+    def __str__(self) -> str:
         val = self.get_value()
         if val is None:
             return str(self.identifier)
 
         return str(self.get_value())
 
-    def __bool__(self):
+    def __bool__(self) -> bool:
         val = self.get_value()
         if val is None:
             msg = "Cannot call __bool__ before calling solve(). "
@@ -174,7 +177,7 @@ class Variable:
         return val
 
 
-def b_and(left, right):
+def b_and(left: Union[bool, Variable], right: Union[bool, Variable]) -> Union[bool, Variable]:
     """
     Computes the binary AND between left and right operands.
 
@@ -185,18 +188,15 @@ def b_and(left, right):
     Returns:
         bool or Variable: the result of the operation.
     """
-    lbool = isinstance(left, bool)
-    rbool = isinstance(right, bool)
-
-    if lbool and rbool:
+    if isinstance(left, bool) and isinstance(right, bool):
         return left and right
 
-    if lbool:
+    if isinstance(left, bool):
         if not left:
             return False
         return right
 
-    if rbool:
+    if isinstance(right, bool):
         if not right:
             return False
         return left
@@ -204,7 +204,7 @@ def b_and(left, right):
     return v_and(left, right)
 
 
-def b_nand(left, right):
+def b_nand(left: Union[bool, Variable], right: Union[bool, Variable]) -> Union[bool, Variable]:
     """
     Computes the binary NAND between left and right operands.
 
@@ -215,26 +215,23 @@ def b_nand(left, right):
     Returns:
         bool or Variable: the result of the operation.
     """
-    lbool = isinstance(left, bool)
-    rbool = isinstance(right, bool)
-
-    if lbool and rbool:
+    if isinstance(left, bool) and isinstance(right, bool):
         return not (left and right)
 
-    if lbool:
+    if isinstance(left, bool):
         if not left:
             return True
-        return v_not(right)
+        return b_not(right)
 
-    if rbool:
+    if isinstance(right, bool):
         if not right:
             return True
-        return v_not(left)
+        return b_not(left)
 
     return v_nand(left, right)
 
 
-def b_or(left, right):
+def b_or(left: Union[bool, Variable], right: Union[bool, Variable]) -> Union[bool, Variable]:
     """
     Computes the binary OR between left and right operands.
 
@@ -245,18 +242,15 @@ def b_or(left, right):
     Returns:
         bool or Variable: the result of the operation.
     """
-    lbool = isinstance(left, bool)
-    rbool = isinstance(right, bool)
-
-    if lbool and rbool:
+    if isinstance(left, bool) and isinstance(right, bool):
         return left or right
 
-    if lbool:
+    if isinstance(left, bool):
         if left:
             return True
         return right
 
-    if rbool:
+    if isinstance(right, bool):
         if right:
             return True
         return left
@@ -264,7 +258,7 @@ def b_or(left, right):
     return v_or(left, right)
 
 
-def b_nor(left, right):
+def b_nor(left: Union[bool, Variable], right: Union[bool, Variable]) -> Union[bool, Variable]:
     """
     Computes the binary NOR between left and right operands.
 
@@ -275,26 +269,23 @@ def b_nor(left, right):
     Returns:
         bool or Variable: the result of the operation.
     """
-    lbool = isinstance(left, bool)
-    rbool = isinstance(right, bool)
-
-    if lbool and rbool:
+    if isinstance(left, bool) and isinstance(right, bool):
         return not (left or right)
 
-    if lbool:
+    if isinstance(left, bool):
         if left:
             return False
-        return v_not(right)
+        return b_not(right)
 
-    if rbool:
+    if isinstance(right, bool):
         if right:
             return False
-        return v_not(left)
+        return b_not(left)
 
     return v_nor(left, right)
 
 
-def b_not(var):
+def b_not(var: Union[bool, Variable]) -> Union[bool, Variable]:
     """
     Computes the binary negation of a variable.
 
@@ -310,7 +301,7 @@ def b_not(var):
     return v_not(var)
 
 
-def b_xor(left, right):
+def b_xor(left: Union[bool, Variable], right: Union[bool, Variable]) -> Union[bool, Variable]:
     """
     Computes the binary XOR between left and right operands.
 
@@ -321,26 +312,23 @@ def b_xor(left, right):
     Returns:
         bool or Variable: the result of the operation.
     """
-    lbool = isinstance(left, bool)
-    rbool = isinstance(right, bool)
-
-    if lbool and rbool:
+    if isinstance(left, bool) and isinstance(right, bool):
         return left ^ right
 
-    if lbool:
+    if isinstance(left, bool):
         if left:
-            return v_not(right)
+            return b_not(right)
         return right
 
-    if rbool:
+    if isinstance(right, bool):
         if right:
-            return v_not(left)
+            return b_not(left)
         return left
 
     return v_xor(left, right)
 
 
-def b_lt(left, right):
+def b_lt(left: Union[bool, Variable], right: Union[bool, Variable]) -> Union[bool, Variable]:
     """
     Computes the less than operation between the left and right operands.
     That is, left < right <=> left == False & right == True
@@ -355,7 +343,7 @@ def b_lt(left, right):
     return b_and(b_xor(left, right), right)
 
 
-def b_le(left, right):
+def b_le(left: Union[bool, Variable], right: Union[bool, Variable]) -> Union[bool, Variable]:
     """
     Computes the less than or equal to operation between the left and right
     operands. That is:
@@ -371,7 +359,7 @@ def b_le(left, right):
     return b_or(b_eq(left, right), right)
 
 
-def b_eq(left, right):
+def b_eq(left: Union[bool, Variable], right: Union[bool, Variable]) -> Union[bool, Variable]:
     """
     Computes the equal to operation between the left and right operands.
 
@@ -385,7 +373,7 @@ def b_eq(left, right):
     return b_xor(b_not(left), right)
 
 
-def b_ne(left, right):
+def b_ne(left: Union[bool, Variable], right: Union[bool, Variable]) -> Union[bool, Variable]:
     """
     Computes the not equal to operation between the left and right operands.
 
@@ -399,7 +387,7 @@ def b_ne(left, right):
     return b_xor(left, right)
 
 
-def b_gt(left, right):
+def b_gt(left: Union[bool, Variable], right: Union[bool, Variable]) -> Union[bool, Variable]:
     """
     Computes the greater than operation between the left and right operands.
     That is, left > right <=> left == True & right == False
@@ -414,7 +402,7 @@ def b_gt(left, right):
     return b_and(b_xor(left, right), left)
 
 
-def b_ge(left, right):
+def b_ge(left: Union[bool, Variable], right: Union[bool, Variable]) -> Union[bool, Variable]:
     """
     Computes the greater than or equal to operation between the left and
     right operands. That is:
@@ -429,7 +417,7 @@ def b_ge(left, right):
     """
     return b_or(b_eq(left, right), left)
 
-def v_and(left, right):
+def v_and(left: Variable, right: Variable) -> Variable:
     """
     Computes the binary AND between left and right operands.
 
@@ -445,7 +433,7 @@ def v_and(left, right):
     return mod.add_constraint(left.identifier, 'and', right.identifier)
 
 
-def v_nand(left, right):
+def v_nand(left: Variable, right: Variable) -> Variable:
     """
     Computes the binary NAND between left and right operands.
 
@@ -461,7 +449,7 @@ def v_nand(left, right):
     return mod.add_constraint(left.identifier, 'nand', right.identifier)
 
 
-def v_or(left, right):
+def v_or(left: Variable, right: Variable) -> Variable:
     """
     Computes the binary OR between left and right operands.
 
@@ -477,7 +465,7 @@ def v_or(left, right):
     return mod.add_constraint(left.identifier, 'or', right.identifier)
 
 
-def v_nor(left, right):
+def v_nor(left: Variable, right: Variable) -> Variable:
     """
     Computes the binary NOR between left and right operands.
 
@@ -493,7 +481,7 @@ def v_nor(left, right):
     return mod.add_constraint(left.identifier, 'nor', right.identifier)
 
 
-def v_not(var):
+def v_not(var: Variable) -> Variable:
     """
     Computes the binary NOT operation on the operands.
 
@@ -506,7 +494,7 @@ def v_not(var):
     return var.model.neg_var(var)
 
 
-def v_xor(left, right):
+def v_xor(left: Variable, right: Variable) -> Variable:
     """
     Computes the binary XOR between left and right operands.
 
