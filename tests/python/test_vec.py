618733d6400fb1e5d9085b43f6fff2b472eea50f
Alexander Scheel <alexander.m.scheel@gmail.com>
2020-12-02 21:57:18 -0500

    Add correctness test for multiplication
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- tests/python/test_vec.py
+++ tests/python/test_vec.py
@@ -25,6 +25,29 @@ def test_add_extensive():
             assert not mod.solve()
 
 
+def test_mul_extensive():
+    k = 4
+    for left in range(0, 1<<k):
+        for right in range(0, 1<<k):
+            mod = cmsh.Model()
+            a = mod.vec(k)
+            b = mod.vec(k)
+            answer = mod.vec(k)
+
+            constraint = (answer == (a * b)) & (a == left) & (b == right)
+            mod.add_assert(constraint)
+            assert mod.solve()
+
+            assert int(a) == left
+            assert int(b) == right
+            assert int(answer) == ((left * right) & ((1 << k) - 1))
+
+            negated = mod.negate_solution(a.variables + b.variables + answer.variables)
+            mod.add_assert(negated)
+
+            assert not mod.solve()
+
+
 def test_subindex():
     mod = cmsh.Model()
     a = mod.vec(10)
